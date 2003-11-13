d5083e31be116ca2f0113abb7cc012b95dd9e040
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2003-11-13 05:30:46 +0000

    Jun's note for 03/11/13 and updated note2.rb -- JM
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1916 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- not2.rb
+++ not2.rb
@@ -1,9 +1,23 @@
-#
+#!/usr/bin/ruby
 # not2.rb
 #
 # test program to start writing conversion from .not to various formats
 #
 
+print <<END_HTML_HEADER
+<HTML>
+  <HEAD><TITLE> #{Time.now} </TITLE></HEAD>
+<BODY>
+<pre>
+END_HTML_HEADER
+
 while gets
    print 
 end
+
+
+print <<END_HTML_FOOTER
+</pre>
+</BODY>
+</HTML>
+END_HTML_FOOTER
