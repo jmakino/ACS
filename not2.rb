c178e7fc258177e86c83d9cb813a099419dcecb8
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2003-11-13 11:33:01 +0000

    labnote-031112.* made as snapshot for labnote
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1869 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- not2.rb
+++ not2.rb
@@ -3,6 +3,8 @@
 #
 # test program to start writing conversion from .not to various formats
 #
+session_time = "-1";
+session_number = -1;
 
 print <<END_HTML_HEADER
 <HTML>
@@ -11,8 +13,26 @@ print <<END_HTML_HEADER
 <pre>
 END_HTML_HEADER
 
-while gets
-   print 
+while str=gets
+  if (str =~ /\\header/)==0
+    
+    print "header found ",  str;
+    entry_name = str[9..999];
+    current_session = str.split(" ")[-1];
+    print current_session, "\n"
+    array=current_session.split("-");
+    current_time=array[0];  current_session_number=array[1];
+
+# single session file= current_time/current_session_number.html
+# monthly file= current_time-current_session_number.html
+# name tag= session_number
+    
+    if current_time != session_time
+      
+    end
+    
+  end;
+   print str
 end
 
 
