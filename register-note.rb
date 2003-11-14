c528a18b648dd66e3596800d377eef7d5224c4be
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2003-11-14 02:02:21 +0000

    naming convention for registe-note.rb changed from 200311-xxx to 2003-11-dd-xxx not2.rb not updated yet --JM
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1873 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- register-note.rb
+++ register-note.rb
@@ -2,24 +2,37 @@
 # register-note.rb
 #
 # 
+def addpreceedingzeros(n,s="")
+zeros="0" * (n - s.length);
+zeros  + s;
+end
+
 sequence_file_name = "sequence.dat";
 sequence_string_length=3;
 global_not_file_name = "all.not";
 
 print "Year", Time.now.year
 print "Month", Time.now.month, "\n";
-dirname = Time.now.year.to_s +  Time.now.month.to_s;
-mask = 0777 ^ File.umask;
+print "day", Time.now.day, "\n";
 
-seqname = dirname + "/"  + sequence_file_name;
+year =  addpreceedingzeros(4,Time.now.year.to_s);
+month = addpreceedingzeros(2,Time.now.month.to_s);
+day =   addpreceedingzeros(2,Time.now.day.to_s);
+dirname = year + "-" +  month;
+mask = 0777 ^ File.umask;
+p dirname
+seqname = dirname + "/"  + sequence_file_name + Time.now.day.to_s;
 unless File.exist?(dirname)
    print "creating the new directory", dirname, "\n";
    Dir.mkdir(dirname, mask);
-   count = 0;
-else
+end
+
+if  File.exist?(seqname)
    seqfile = open(seqname, "r");
    count = seqfile.gets.to_i;
    seqfile.close;
+else
+   count=0
 end
 sequence_number = count + 1;
 seqfile = open(seqname, "w+");
@@ -33,19 +46,19 @@ STDERR.print "New sequence count = ", count, "\n";
 # Date, Name of the person, sequence number 
 #
 # sequence number here includes year, month and zeros preceeding
-# the real number, like 20031113-001
+# the real number, like 2003-11-13-001
 #
 header = Time.now;
 name = `finger -l #username | head -1`.split($/);
 realname = name[0];
 realname = realname[realname.index("Name:")+6..999];
-sequence_string = sequence_number.to_s;
-preceeding_zeros="0" * (sequence_string_length - sequence_string.length);
-short_sequence_string=  preceeding_zeros + sequence_string;
+sequence_string = addpreceedingzeros(sequence_string_length,
+                 sequence_number.to_s)
+short_sequence_string=  day + "-" + sequence_string;
 long_sequence_string= dirname + "-" + short_sequence_string;
 
-header = "\\header " + header.to_s + " "+ realname+ " Session No. = "+ long_sequence_string;
-
+header = "\\header " + header.to_s + " "+ realname+ \
+   " Session No. = "+ long_sequence_string;
 
 sequence_lab_file_name = seqname = dirname + "/"  + short_sequence_string;
 STDERR.print "seq file name = ", sequence_lab_file_name, "\n";
