8b289aba80c446b89f14b9c3e5ba0a515f5c1785
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2003-11-14 08:15:17 +0000

    script to convert "all.not" for emacs outline mode
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1875 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ not2outline.rb
@@ -0,0 +1,20 @@
+#!/usr/local/bin/ruby -w
+#
+#  this script converts the "all.not" file to usage in emacs outline mode
+#
+#  usage:  not2outline.rb < all.not > allout.not
+#          emacs allout.not
+#
+
+while s = gets
+  if s == "\n"
+    print " \n"
+  else
+    print s
+  end
+  if s =~ /^\\header/
+    s = gets until s != "\n"
+    print "\n"
+    print "* ", s
+  end
+end
