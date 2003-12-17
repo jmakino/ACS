e2d2ec1937b04e4f12432cec889eec7fc4d93be6
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2003-12-17 08:46:09 +0000

    nbody_sh1_c.rb and pairwize.c implements pairwize force calculation in C. -- JM
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1894 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- ruby/testclass.h
+++ ruby/testclass.h
@@ -1,14 +1,5 @@
 /* File : test.h */
 
-class Shape {
- public:
+typedef struct Shapestruct {
     double  x, y;   
-    Shape(double xx=0.0, double yy=0.0){
-	x = xx;
-	y = yy;
-    }
-    void    move(double dx, double dy){
-	x += dx;
-	y += dy;
-    };
-};
+}Shape, *Shapeptr;
