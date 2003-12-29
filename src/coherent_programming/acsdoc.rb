93799ffbe2d077be37eac2ad003525ee23232f68
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2003-12-29 17:44:57 +0000

    src/coherent_programming/acsdoc.rb implements :segment start/end: directives
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@98 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- src/coherent_programming/acsdoc.rb
+++ src/coherent_programming/acsdoc.rb
@@ -15,54 +15,108 @@
 #
 #==============================================================================
 
-def prep_cp(infile, outfile)
-  if not ifile = open(infile, "r")
-    raise "#{infile} does not exist"
+module Acsdoc
+  def prep_cp(infile, outfile)
+    begin
+      ifile = open(infile, "r")
+    rescue
+      raise "#{infile} does not exist"
+    end
+    ofile = open(outfile, "w+")
+    while s = ifile.gets
+      s.gsub!(/<p>/, '<i>[Jun: ')
+      s.gsub!(/<\/p>/, ' -- Piet] </i>')
+      s.gsub!(/<j>/, '<i>[Piet ')
+      s.gsub!(/<\/j>/, ' -- Jun] </i>')
+      if s =~ /:in.*code:/ and s.index("\":inccode:\"")==nil
+	s.sub!(/:in.*code:/, ':include:')
+	ofile.print "---\n"
+	ofile.print s
+	ofile.print "---\n"
+      else
+	ofile.print s
+      end
+    end
+    ifile.close
+    ofile.close
   end
-  ofile = open(outfile, "w+")
-  while s = ifile.gets
-    s.gsub!(/<p>/, '<i>[Jun: ')
-    s.gsub!(/<\/p>/, ' -- Piet] </i>')
-    s.gsub!(/<j>/, '<i>[Piet ')
-    s.gsub!(/<\/j>/, ' -- Jun] </i>')
-    if s =~ /:in.*code:/
-      s.sub!(/:in.*code:/, ':include:')
-      ofile.print "---\n"
-      ofile.print s
-      ofile.print "---\n"
-    else
-      ofile.print s
+
+  # :segment start: prep_rb
+  def prep_rb(infile)
+    # :segment start: open-input-file
+    begin
+      ifile = open(infile, "r")
+    rescue
+      raise "#{infile} does not exist"
+    end
+    # :segment end:
+    segment_level = 0
+    ofile = Array.new;
+    oname = Array.new;
+    while s = ifile.gets
+      if loc = s.index(":segment start:")  and s.index("\":segment start:\"")==nil
+	segment_name = s[loc,s.length].split[2];
+	print segment_name;
+	outfile = File.dirname(infile)+"/."+File.basename(infile) +
+	  "-" + segment_name
+	if oname[0...segment_level].index(outfile) != nil
+	  raise "Too many #{s} for file #{infile} line #{ARGF.lineno}"
+	end
+	oname[segment_level]=outfile
+	ofile[segment_level] = open(outfile, "w+")
+	segment_level += 1
+      elsif  s.index(":segment end:") and  s.index("\":segment end:\"")  ==nil
+	segment_level -= 1
+	if (segment_level >= 0)
+	  ofile[segment_level].close
+	else
+	  raise "Too many #{s} for file #{infile} line #{ARGF.lineno}"
+	end
+      else
+	for i in 0...segment_level do
+	  ofile[i].print s
+	end
+      end
+    end
+    ifile.close
+  end
+  # :segment end: prep_rb
+
+  def acsdoc()
+    
+    del_flag = true
+    del_file_list = Array.new
+    
+    ARGV.collect! do |a|
+      if a =~ /\.cp$/
+	dot_a = File.dirname(a)+"/."+File.basename(a);
+	prep_cp(a, dot_a)
+	a = dot_a
+	del_file_list.push(dot_a)
+      elsif a =~ /\.rb$/
+	prep_rb(a)
+	a
+      elsif a == "--keep-dot-files"
+	del_flag = false
+	a = ""
+      end
+      a
+    end
+    
+    system("rdoc #{ARGV.join(" ")}")
+    
+    if del_flag
+      del_file_list.each do |f|
+	File.delete(f)
+      end
     end
   end
-  ifile.close
-  ofile.close
 end
 
-del_flag = true
+include Acsdoc
+acsdoc
 
-del_file_list = Array.new
 
-ARGV.collect! do |a|
-  if a =~ /\.cp$/
-    dot_a = File.dirname(a)+"/."+File.basename(a);
-#    dot_a = '.'+a
-    prep_cp(a, dot_a)
-    a = dot_a
-    del_file_list.push(dot_a)
-  elsif a == "--keep-dot-files"
-    del_flag = false
-    a = ""
-  end
-  a
-end
-
-system("rdoc #{ARGV.join(" ")}")
-
-if del_flag
-  del_file_list.each do |f|
-    File.delete(f)
-  end
-end
 
 
 
