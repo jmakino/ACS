1ae3eff2555f5d7cb9803f556b34b3ce549737ff
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2003-12-31 05:12:48 +0000

    added coherent_programming/acsmaildoc.rb, for processing mails for lab notes -- JM
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@105 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ src/coherent_programming/acsmaildoc.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/ruby
+=begin
+  acsmaildoc.rb
+=end
+
+$docroot = "/usr2/acs/acs/not/Mail"
+$maildir = "/home/not/Mail/inbox"
+$seqfile = $docroot + "/seq"
+module Acsmaildoc
+  def prep_cp(infile, outfile)
+    begin
+      ifile = open(infile, "r")
+    rescue
+      raise "#{infile} does not exist"
+    end
+    ofile = open(outfile, "w+")
+    while s = ifile.gets
+      s.gsub!(/<p>/, '<i>[Jun: ')
+      s.gsub!(/<\/p>/, ' -- Piet] </i>')
+      s.gsub!(/<j>/, '<i>[Piet ')
+      s.gsub!(/<\/j>/, ' -- Jun] </i>')
+      if s =~ /:in.*code:/ and s.index("\":inccode:\"")==nil
+	s.sub!(/:in.*code:/, ':include:')
+	ofile.print "---\n"
+	ofile.print s
+	ofile.print "---\n"
+      else
+	ofile.print s
+      end
+    end
+    ifile.close
+    ofile.close
+  end
+
+  # :segment start: prep_rb
+  def process_mailfile(seqnum)
+    infile = open(seqname = $maildir + "/" + seqnum.to_a, "r")
+    inheader = 1
+    while s=infile.gets
+      if inheader == 1
+	s.chomp!
+	if s =~ /^Date:/
+	  time = Time.parse(s[5,s.length])
+	  print "time for file ", seqnum = $seq, time
+	end
+	if s =~ /^From:/ 
+	  from = s
+	end
+	if s.empty? 
+	  inheader = 0
+	end
+      end
+    end
+    infile.close
+  end
+  # :segment end: prep_rb
+  def acsmaildoc()
+    begin
+      ifile = open($seqfile, "r")
+      seqnum = ifile.gets.to_i
+      ifile.close
+    rescue
+      ofile = open($seqfile, "w+")
+      ofile.print "1"
+      ofile.close
+      retry
+    end
+    print "current seq = ", seqnum, "\n";
+    while  File.exist?(seqname = $maildir + "/" + seqnum.to_a) do
+      process_mailfile(seqnum)
+      seqnum += 1
+    end
+    ofile = open($seqfile, "w+")
+    ofile.print seqnum, "\n"
+    ofile.close
+  end
+end
+
+include Acsmaildoc
+acsmaildoc
+
+
+
+
+
+
+
