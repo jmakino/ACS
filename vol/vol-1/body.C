d664fc8aebee8587cf3f88ecfe770735dde993e2
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-03-21 17:13:34 +0000

    A simple C++ sample for body definition, body.C, added -- JM
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@234 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ vol/vol-1/body.C
@@ -0,0 +1,32 @@
+#include  <iostream>
+using namespace std;
+class body
+{
+private:
+    double pos[3];
+    double vel[3];
+    double mass;
+public:
+    body(double inmass, double inpos[3], double invel[3]){
+	mass = inmass;
+	for(int i=0;i<3;i++){
+	    pos[i] = inpos[i];
+	    vel[i] = invel[i];
+	}
+
+    }
+    void print(){
+	cout << mass <<endl;
+	cout << pos[0] << " "<< pos[1] << " "<< pos[2]   <<endl;
+	cout << vel[0] << " "<< vel[1] << " "<< vel[2]   <<endl;
+    }
+	
+};
+main()
+{
+    double zero3[3]={0.0,0.0,0.0};
+    body x =  body(0.0,zero3,zero3);
+    x.print();
+}
+
+
