1b731369bebe60617244722af6ee886b58f6fba0
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-03-25 19:17:15 +0000

    new examples
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@258 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ vol/vol-1/body2.rb
@@ -0,0 +1,9 @@
+class Body
+
+  attr_accessor :mass, :pos, :vel
+
+  def initialize(mass = 0, pos = [0,0,0], vel = [0,0,0])
+    @mass, @pos, @vel = mass, pos, vel
+  end
+
+end
---
+++ vol/vol-1/body4.rb
@@ -0,0 +1,15 @@
+class Body
+
+  attr_accessor :mass, :pos, :vel
+
+  def initialize(mass = 0, pos = [0,0,0], vel = [0,0,0])
+    @mass, @pos, @vel = mass, pos, vel
+  end
+
+  def to_s
+    "  mass = " + @mass.to_s + "\n" +
+    "   pos = " + @pos.join(", ") + "\n" +
+    "   vel = " + @vel.join(", ") + "\n"
+  end
+
+end
