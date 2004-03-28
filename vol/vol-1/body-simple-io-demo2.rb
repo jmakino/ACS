87c313883db11d13d00f6c8f188fe113396638ec
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-03-28 20:59:15 +0000

    scripts for volume 1
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@307 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ vol/vol-1/body-simple-io-demo2.rb
@@ -0,0 +1,11 @@
+#<i>Demo for +simple_read+ and +simple_print+ methods in Body</i>
+
+require "body8.rb"
+
+#:segment start: demo
+b = Body.new()
+
+b.simple_read
+
+b.simple_print
+#:segment end:
---
+++ vol/vol-1/euler2.rb
@@ -0,0 +1,21 @@
+require "body.rb"
+require "vector.rb"
+
+include Math
+
+dt = 0.01            # time step size
+ns = 100             # number of time steps
+
+b = Body.new
+b.simple_read
+
+ns.times do
+  r2 = 0
+  b.pos.each {|p| r2 += p*p}
+  r3 = r2 * sqrt(r2)
+  acc = b.pos.map { |x| -b.mass * x/r3 }
+  b.pos.each_index { |k| b.pos[k] += b.vel[k] * dt }
+  b.vel.each_index { |k| b.vel[k] += acc[k] * dt }
+end
+
+b.pp
