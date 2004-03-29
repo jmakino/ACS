5be1364fdf9a29e64601698d1fbfb5b1db72f01d
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-03-29 16:26:15 +0000

    restored
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@318 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ vol/vol-1/vector1.rb
@@ -0,0 +1,17 @@
+ class Vector < Array
+  def +(a)
+    sum=[]
+    self.each_index{|k| sum[k]=self[k]+a[k]}
+    sum
+  end
+  def *(a)
+    if a.class == Vector              # inner product
+      product=0
+      self.each_index{|k| product += self[k]*a[k]}
+    else
+      product=[]                     # scalar product
+      self.each_index{|k| product[k]=self[k]*a}
+    end
+    product
+  end
+end
