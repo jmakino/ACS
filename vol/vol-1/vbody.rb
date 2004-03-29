edea9d067e7e964b92a95878ba8b9045a299f952
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-03-29 13:36:48 +0000

    new vector definition
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@311 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ vol/vol-1/vbody.rb
@@ -0,0 +1,33 @@
+require "vector.rb"
+
+class Body
+
+  attr_accessor :mass, :pos, :vel
+
+  def initialize(mass = 0, pos = Vector[0,0,0], vel = Vector[0,0,0])
+    @mass, @pos, @vel = mass, pos, vel
+  end
+
+  def to_s
+    "  mass = " + @mass.to_s + "\n" +
+    "   pos = " + @pos.join(", ") + "\n" +
+    "   vel = " + @vel.join(", ") + "\n"
+  end
+
+  def pp               # pretty print
+    print to_s
+  end
+
+  def simple_print
+    printf("%24.16e\n", @mass)
+    @pos.each { |x| printf("%24.16e", x) } ; print "\n"
+    @vel.each { |x| printf("%24.16e", x) } ; print "\n"
+  end
+
+  def simple_read
+    @mass = gets.to_f
+    @pos = gets.split.map{|x| x.to_f}.to_v
+    @vel = gets.split.map{|x| x.to_f}.to_v
+  end
+
+end
