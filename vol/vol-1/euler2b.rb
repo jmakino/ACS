8021fa480b4af88692fcefc2193f50accded9631
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-03-29 22:15:20 +0000

    more helper files for chapter 16
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@323 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ vol/vol-1/euler2b.rb
@@ -0,0 +1,19 @@
+require "vbody2.rb"
+
+include Math
+
+dt = 0.01            # time step size
+ns = 100             # number of time steps
+
+b = Body.new
+b.simple_read
+
+ns.times do
+  r2 = b.pos * b.pos
+  r3 = r2 * sqrt(r2)
+  acc = b.pos * (-b.mass / r3)
+  b.pos += b.vel * dt
+  b.vel += acc * dt
+end
+
+b.pp
