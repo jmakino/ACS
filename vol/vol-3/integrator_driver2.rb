31f404fec4b3452eccb245adcb32e14fb3885eea
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-05-28 09:08:45 +0000

    still splitting old volume 2
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@488 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ vol/vol-3/integrator_driver2.rb
@@ -0,0 +1,22 @@
+require "rkbody.rb"
+
+include Math
+
+dt = 0.001           # time step
+dt_dia = 10          # diagnostics printing interval
+dt_out = 10          # output interval
+dt_end = 10          # duration of the integration
+#method = "forward"   # integration method
+#method = "leapfrog"  # integration method
+method = "rk2"       # integration method
+#method = "rk4"       # integration method
+
+STDERR.print "dt = ", dt, "\n",
+      "dt_dia = ", dt_dia, "\n",
+      "dt_out = ", dt_out, "\n",
+      "dt_end = ", dt_end, "\n",
+      "method = ", method, "\n"
+
+b = Body.new                                                    #1
+b.simple_read                                                   #1
+b.evolve(method, dt, dt_dia, dt_out, dt_end)                    #1
