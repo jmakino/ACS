387cbaed7e1798cd9c4a21401aeaeae7f67bd6e3
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-05-31 00:52:52 +0000

    more text
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@571 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ kali/vol/vol-3/rknbody8e_driver.rb
@@ -0,0 +1,26 @@
+require "rknbody8.rb"
+
+include Math
+
+dt = 0.0001          # time step
+dt_dia = 2           # diagnostics printing interval
+dt_out = 10          # output interval
+dt_end = 2           # duration of the integration
+init_out = false     # initial output requested ?
+x_flag = false       # extra diagnostics requested ?
+#method = "forward"  # integration method
+#method = "leapfrog" # integration method
+#method = "rk2"      # integration method
+method = "rk4"       # integration method
+
+STDERR.print "dt = ", dt, "\n",
+      "dt_dia = ", dt_dia, "\n",
+      "dt_out = ", dt_out, "\n",
+      "dt_end = ", dt_end, "\n",
+      "init_out = ", init_out, "\n",
+      "x_flag = ", x_flag, "\n",
+      "method = ", method, "\n"
+
+nb = Nbody.new
+nb.simple_read
+nb.evolve(method, dt, dt_dia, dt_out, dt_end, init_out, x_flag)
