3af4492b8ed1dabfb9be7f32b3f7207bb0b25fec
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-06-03 04:25:14 +0000

    more text
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@602 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/vol/vol-2/rk6body.rb
+++ kali/vol/vol-2/rk6body.rb
@@ -70,7 +70,7 @@ class Body
     @vel = vel + (a0+a1*4+a2)*(1/6.0)*dt                                    #1
   end
 
-  def rk6(dt)                         # yoshida6b, from Akagi book
+  def rk6(dt)
     d = [0.784513610477560e0, 0.235573213359357e0, -1.17767998417887e0,
          1.31518632068391e0]
     for i in 0..2 do leapfrog(dt*d[i]) end
