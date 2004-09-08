9264db72a291405b29b0fc541afa2b067c51e1bf
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-09-08 21:12:16 +0000

    moving directory names
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@809 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ kali/vol/vol-10/vector.rb
@@ -0,0 +1,43 @@
+class Vector < Array
+  def +(a)
+    sum = xxxxx	Vector.new
+    self.each_index{|k| sum[k] = self[k]+a[k]}
+	sum
+  end
+  def -(a)
+    diff = Vector.new
+    self.each_index{|k| diff[k] = self[k]-a[k]}
+    diff
+  end
+  def -@
+    self.map{|x| -x}.to_v
+  end
+  def +@
+    self
+  end
+  def *(a)
+    if a.class == Vector              # inner product
+      product = 0
+      self.each_index{|k| product += self[k]*a[k]}
+    else
+      product = Vector.new           # scalar product
+      self.each_index{|k| product[k] = self[k]*a}
+    end
+    product
+  end
+  def /(a)
+    if a.class == Vector
+      raise
+    else
+      quotient = Vector.new           # scalar quotient
+      self.each_index{|k| quotient[k] = self[k]/a}
+    end
+    quotient
+  end
+end
+
+class Array
+  def to_v
+    Vector[*self]
+  end
+end
