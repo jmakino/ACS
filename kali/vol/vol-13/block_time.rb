6c59f46588ab5c470007b141f7395c00b7ff34b6
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-09-21 23:40:59 +0000

    more text
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@835 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/vol/vol-13/block_time.rb
+++ kali/vol/vol-13/block_time.rb
@@ -65,14 +65,39 @@ class Block_time
     end
   end
 
-#  def +(a)
-#    if a.class == Block_time
-#      sum = Block_time.new
-#      i = 
-#
-#
-#
-#      sum.time_int = @time_int + a.time_int
-      
+  def +(a)
+    if a.class == Block_time
+      sum = Block_time.new
+      expand
+      a.expand
+      i = TIME_ARRAY_MAX_LENGTH - 1
+      carry = 0
+      while i >= 0
+        total = @time_array[i] + a.time_array[i] + carry
+        case total
+          when 0
+            sum.time_array[i] = 0
+            carry = 0
+          when 1
+            sum.time_array[i] = 1
+            carry = 0
+          when 2
+            sum.time_array[i] = 0
+            carry = 1
+          when 3
+            sum.time_array[i] = 1
+            carry = 1
+        end
+        i -= 1
+      end
+      sum.time_int = @time_int + a.time_int + carry
+      sum.contract
+      return sum
+    end
+  end
+
+  def to_s
+    "{" + @time_int.to_s + ", [" + @time_array.join(", ") + "]}\n"
+  end
 
 end
