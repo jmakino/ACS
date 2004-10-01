61b1b822ea17655e5631bcd694a83bc027a2866f
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-10-01 17:36:56 +0000

    first draft
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@857 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/vol/vol-13/world1.rb
+++ kali/vol/vol-13/world1.rb
@@ -6,6 +6,66 @@ class Worldpoint
 
   def propagate(era, wl, dt_param)
     ss = era.make_snapshot(wl, @next_time)
+    wp = ss.body.shift
+    wp.acc, wp.jerk = wp.get_acc_and_jerk(ss)
+    w = interpolate(wp, wp.time)
+    w.acc, w.jerk = wp.acc, wp.jerk
+    w.next_time = w.time + w.collision_time_scale(ss) * dt_param
+    w
+  end
+
+  def get_acc_and_jerk(snapshot)
+    a = j = @pos*0                         # null vectors of the correct length
+    snapshot.body.each do |b|
+      r = b.pos - @pos
+      r2 = r*r
+      r3 = r2*sqrt(r2)
+      v = b.vel - @vel
+      a += r*(b.mass/r3)
+      j += (v-r*(3*(r*v)/r2))*(b.mass/r3)
+    end
+    [a, j]
+  end    
+
+  def collision_time_scale(snapshot)
+    time_scale_sq = 1e30                         # square of time scale value
+    snapshot.body.each do |b|
+      r = b.pos - @pos
+      v = b.vel - @vel
+      r2 = r*r
+      v2 = v*v
+      estimate_sq = r2 / v2              # [distance]^2/[velocity]^2 = [time]^2
+      if time_scale_sq > estimate_sq
+        time_scale_sq = estimate_sq
+      end
+      a = (@mass + b.mass)/r2
+      estimate_sq = sqrt(r2)/a           # [distance]/[acceleration] = [time]^2
+      if time_scale_sq > estimate_sq
+        time_scale_sq = estimate_sq
+      end
+    end
+    sqrt(time_scale_sq)                  # time scale value
+  end
+
+  def ekin                               # kinetic energy
+    0.5*@mass*(@vel*@vel)
+  end
+
+  def epot(body_array)                   # potential energy
+    p = 0
+    body_array.each do |b|
+      unless b == self
+        r = b.pos - @pos
+        p += -@mass*b.mass/sqrt(r*r)
+      end
+    end
+    p
+  end
+
+  def ppx                    # pretty print, with extra information (acc, jerk)
+    STDERR.print to_s
+    STDERR.print "   acc = " @acc.join(", ") + "\n"
+    STDERR.print "   jerk = " + @jerk.join(", ") + "\n"
   end
 
   def read
@@ -20,16 +80,44 @@ class Worldpoint
     @vel.each{|x| printf("%24.16e", x)}; print "\n"
   end
 
-  def extrapolate
-
-#
-      if wpl.next_time < time
-        raise "wpl.next_time = " + wpl.next_time.to_s + " < " + time.to_s
-      end
-#
+  def extrapolate(t)
+    if t > @next_time
+      raise "t = " + t.to_s + " > @next_time = " + @next_time.to_s + "\n"
+    end
+    wp = Worldpoint.new
+    wp.mass = @mass
+    wp.time = t
+    dt = t - @time
+    wp.pos = @pos + @vel*dt + @acc*(dt*dt/2.0) + @jerk*(dt*dt*dt/6.0)
+    wp.vel = @vel + @acc*dt + @jerk*(dt*dt/2.0)
+    wp
   end
 
-  def interpolate
+  def interpolate(other, t)
+    wp = Worldpoint.new
+    wp.mass = @mass
+    wp.time = t
+    dt = other.time - @time
+    snap = ((@acc - other.acc)*(-6) - (@jerk*2 + other.jerk)*2*dt)/(dt*dt)
+    crackle = ((@acc - other.acc)*12 + (@jerk + other.jerk)*6*dt)/(dt*dt*dt)
+    dt = t - @time
+    wp.pos = @pos + @vel*dt + @acc*(dt**2/2.0) + @jerk*(dt**3/6.0) +
+             snap*(dt**4/24.0) + crackle*(dt**5/120.0)
+    wp.vel = @vel + @acc*dt + @jerk*(dt**2/2.0) + snap*(dt**3/6.0) + 
+             crackle*(dt**4/24.0)
+    wp
+  end
+
+  def clone
+    c = Worldpoint.new
+    c.mass = @mass
+    c.pos = @pos*1             # silly, but it works ; to be cleaned up
+    c.vel = @vel*1             # by overloading the = operator ; without 
+    c.acc = @acc*1             # this silly trick, no deep vector copy !!
+    c.jerk = @jerk*1
+    c.time = @time
+    c.next_time = @next_time
+    c
   end
 
 end
@@ -56,6 +144,12 @@ class Worldline
         end
       end
     end
+  end
+
+  def next_worldline
+    wl = Worldline.new
+    wl.worldpoint[0] = @worldpoint.last.clone
+    wl
   end
 
 #  def read                               # for self-documenting data
@@ -87,24 +181,29 @@ class Worldera
                                          # of the contents, i.e. the worldlines
   def initialize
     @worldline = []
-    @nsteps = 0
-  end
-
-  def startup(dt_param)
   end
 
   def find_next_worldline
   end
 
   def evolve(c)
-    startup(c.dt_param) if not @worldline[0].worldpoint[0].acc
+    nsteps = 0
     @end_time = @start_time + c.dt_era
     time = @start_time
     while time < @end_time
       nwl = find_next_worldline
       time = nwl.worldpoint.last.next_time
       nwl.extend(self, c.dt_param)
-      @nsteps += 1
+      nsteps += 1
+    end
+    [next_era, nsteps]
+  end
+
+  def next_era
+    e = Worldera.new
+    e.start_time = @end_time
+    @wordline.each do |wl|
+      e.worldline.push(wl.next_worldline)
     end
   end
 
@@ -120,10 +219,11 @@ class Worldera
     end
   end
 
-  def write_snapshot
-  end
-
-  def write_diagnostics
+  def write_diagnostics(t, nsteps, initial_energy, x_flag)
+    STDERR.print "at time t = #{sprintf("%g", t)} "
+    STDERR.print "(from interpolation after #{nsteps} steps "
+    STDERR.print "to time #{sprintf("%g", @end_time)}):"
+    make_snapshot(nil, t).write_diagnostics(initial_energy, x_flag)
   end
 
 #  def read          # for self-documenting data, either Worldera or Snapshot
@@ -139,12 +239,10 @@ class Worldera
     end
   end
 
-  def write_snapshot
+  def write_snapshot(t)
     print @worldline.size, "\n"
-    printf("%24.16e\n", @worldline[0].worldpoint.last.time)
-    @worldline.each do |wl|
-      wl.write_worldpoint
-    end
+    printf("%24.16e\n", t)
+    make_snapshot(nil, t).write
   end
 
   def write
@@ -159,20 +257,35 @@ end
 
 class World
 
+  def startup(dt_param)
+    @era.wordline.each do |wl|
+      wp = wl.worldpoint[0]
+      ss = era.make_snapshot(wl, wp.time)
+      ss.body.shift
+      wp.acc, wp.jerk = wp.get_acc_and_jerk(ss)
+      wp.next_time = wp.time + wp.collision_time_scale(ss) * dt_param
+    end
+    ss = era.make_snapshot(nil, @era.start_time)
+    ss.ekin + ss.epot
+  end
+
   def evolve(c)
+    initial_energy = startup(c.dt_param)
     time = @era.start_time
-    @era.write_diagnostics(time, c.x_flag)
+    nsteps = 0
+    @era.write_diagnostics(time, nsteps, initial_energy, c.x_flag)
     t_dia = time + c.dt_dia
     t_out = time + c.dt_out
     t_end = time + c.dt_end
-    @era.write_snapshot if c.init_out
+    @era.write_snapshot(time) if c.init_out
     while @era.start_time < t_end
-      @new_era = @era.evolve(c)
+      @new_era, dn = @era.evolve(c)
+      nsteps += dn
       @old_era = @era
       @era = @new_era
       time = @old_era.end_time
       while time >= t_dia
-        @old_era.write_diagnostics(t_dia, c.x_flag)
+        @old_era.write_diagnostics(t_dia, nsteps, initial_energy, c.x_flag)
         t_dia += c.dt_dia
       end
       if time >= t_out
@@ -186,8 +299,8 @@ class World
     @era.read_snapshot
   end
 
-  def write_snapshot
-    @era.write_snapshot
+  def write_snapshot(time)
+    @era.write_snapshot(time)
   end
 
   def write
@@ -204,6 +317,45 @@ class Worldsnapshot
 
   def initialize
     @body = []
+  end
+
+  def ekin                        # kinetic energy
+    e = 0
+    @body.each{|b| e += b.ekin}
+    e
+  end
+
+  def epot                        # potential energy
+    e = 0
+    @body.each{|b| e += b.epot(@body)}
+    e/2                           # pairwise potentials were counted twice
+  end
+
+  def write_diagnostics(initial_energy, x_flag)
+    e0 = initial_energy
+    STDERR.print <<END
+  E_kin = #{sprintf("%.3g", ekin)} ,\
+ E_pot =  #{sprintf("%.3g", epot)} ,\
+ E_tot = #{sprintf("%.3g", etot)}
+             E_tot - E_init = #{sprintf("%.3g", etot - e0)}
+  (E_tot - E_init) / E_init = #{sprintf("%.3g", (etot - e0)/e0 )}
+END
+    if x_flag
+      STDERR.print "  for debugging purposes, here is the internal data ",
+                   "representation:\n"
+      ppx
+    end
+  end
+
+  def ppx                          # pretty print, with extra information (acc)
+    print "     N = ", @body.size, "\n"
+    @body.each{|b| b.ppx(@body)}
+  end
+
+  def write
+    @body.each do |b|
+      b.write
+    end
   end
 
 end
@@ -330,6 +482,6 @@ clop = parse_command_line(options_text, true)
 
 w = World.new
 w.read_snapshot                            # for now; "read" w. self-doc. data
-w.write_snapshot
+w.write_snapshot(0)
 #w.write
 w.evolve(clop)
