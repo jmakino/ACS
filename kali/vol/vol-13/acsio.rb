60f59cac82022fc5b50b0933252ec0db1b51a785
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2004-10-07 21:40:18 +0000

    acsio seems to work!!
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@879 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/vol/vol-13/acsio.rb
+++ kali/vol/vol-13/acsio.rb
@@ -1,10 +1,24 @@
+class Fixnum
+  def Fixnum.acs_parse(s, indent)
+# check indentation here first)
+    s.to_i
+  end
+end
+
+class Bignum
+  def Bignum.acs_parse(s, indent)
+# check indentation here first)
+    s.to_i
+  end
+end
+
 class Float
   def to_acs_special_s(precision, indent)
     " " * indent + sprintf(" %#{precision+8}.#{precision}e", self) + "\n"
   end
-  def Float.acs_parse(a, indent)
+  def Float.acs_parse(s, indent)
 # check indentation here first)
-    a.to_f
+    s.to_f
   end
 end
 
@@ -12,6 +26,11 @@ class Vector
   def to_acs_special_s(precision, indent)
     " " * indent +
       self.map{|x| sprintf(" %#{precision+8}.#{precision}e", x)}.join + "\n"
+  end
+  def Vector.acs_parse(s, indent)
+# check indentation here first)
+# and this here below should go partially to vector.rb
+    s.split.map{|x| x.to_f}.to_v
   end
 end
 
@@ -24,6 +43,20 @@ class String
     end
     s + "\n"
   end
+  def String.acs_parse(a, indent)
+    s = ""
+    loop do
+      line = a.shift
+      return s.chomp unless line
+      blanks = count_initial_blanks(line)
+      if blanks < indent
+        a.unshift(line)
+        return s.chomp
+      else
+        s += line[indent...line.size] + "\n"
+      end
+    end
+  end
 end
 
 class Array
@@ -33,6 +66,43 @@ class Array
       s += self[i].to_acs_s(name+"["+i.to_s+"]", precision, indent, add_indent)
     end
     s
+  end
+  def Array.acs_parse(a, base_indent)
+    ar = []
+    loop do
+      first_line = a.shift
+      return ar unless first_line
+      indent = count_initial_blanks(first_line)
+      if indent <= base_indent
+        a.unshift(first_line)
+        return ar
+      end      
+      first_line_word = first_line.split
+      class_name = first_line_word[0]
+      variable_name = first_line_word[1]
+      if class_name == "Float"
+        eval("ar.push(Float.acs_parse(a.shift, indent))")
+      elsif class_name == "Fixnum"
+        eval("ar.push(Fixnum.acs_parse(a.shift, indent))")
+      elsif class_name == "Bignum"
+        eval("ar.push(Bignum.acs_parse(a.shift, indent))")
+      elsif class_name == "Vector"
+        eval("ar.push(Vector.acs_parse(a.shift, indent))")
+      elsif class_name == "Array"
+        eval("ar.push(Array.acs_parse(a.shift, indent))")
+      elsif class_name == "String"
+        add_indent = first_line_word[2].to_i
+        eval("ar.push(String.acs_parse(a, indent + add_indent))")
+      else
+        begin
+          eval("x = #{class_name}.new")
+        rescue NameError
+          define_class(class_name)
+          retry
+        end
+        eval("ar.push(x.acs_parse(a, indent))")
+      end
+    end
   end
 end
 
@@ -85,7 +155,7 @@ module ACS_IO
     s = file.gets
     while s =~ /^\s*$/
       s = file.gets
-    end 
+    end
     return nil if not s
     raise "Not an ACS file" if s != ACS_HEADER
     contents = ""
@@ -102,8 +172,7 @@ module ACS_IO
     return nil if not s
     a = s.split("\n")
     first_line = a.shift
-    first_line =~ /^ */
-    indent = $&.size
+    indent = count_initial_blanks(first_line)
     top_class_name = first_line.strip
     begin
       tc = eval(top_class_name).new
@@ -130,51 +199,46 @@ module ACS_IO
     end
   end
 
-  def to_do
-    
+  def count_initial_blanks(s)
+    s =~ /^ */
+    $&.size
   end
 
   def acs_parse(a, base_indent)
-    first_line = a.shift
-    first_line =~ /^ */
-    indent = $&.size
-    first_line_word = first_line.split
-    class_name = first_line_word[0]
-    variable_name = first_line_word[1]
-    if class_name == "Float"
-      eval("@#{variable_name} = Float.acs_parse(a.shift, indent)")
-    else
-    begin
-      eval("@#{variable_name} = #{class_name}.new")
-      rescue NameError
-      define_class(class_name)
-      exit
-      retry
+    loop do
+      first_line = a.shift
+      return self unless first_line
+      indent = count_initial_blanks(first_line)
+      if indent <= base_indent
+        a.unshift(first_line)
+        return self
+      end      
+      first_line_word = first_line.split
+      class_name = first_line_word[0]
+      variable_name = first_line_word[1]
+      if class_name == "Float"
+        eval("@#{variable_name} = Float.acs_parse(a.shift, indent)")
+      elsif class_name == "Fixnum"
+        eval("@#{variable_name} = Fixnum.acs_parse(a.shift, indent)")
+      elsif class_name == "Bignum"
+        eval("@#{variable_name} = Bignum.acs_parse(a.shift, indent)")
+      elsif class_name == "Vector"
+        eval("@#{variable_name} = Vector.acs_parse(a.shift, indent)")
+      elsif class_name == "Array"
+        eval("@#{variable_name} = Array.acs_parse(a, indent)")
+      elsif class_name == "String"
+        add_indent = first_line_word[2].to_i
+        eval("@#{variable_name} = String.acs_parse(a, indent + add_indent)")
+      else
+        begin
+          eval("@#{variable_name} = #{class_name}.new")
+        rescue NameError
+          define_class(class_name)
+          retry
+        end
+        eval("@#{variable_name}.acs_parse(a, indent)")
+      end
     end
-    end
-p self
-exit
-    tc.acs_parse(a, indent)
-
-
-    first_line = a.shift.split
-    io_name_okay?(first_line[0])
-    name = first_line[1]
-    first_line =~ /^ */
-    indent = $&.size
-    line = a.shift
-    return nil if not line
-    raise if line !~ /^ */    
-    return a.unshift(line) if $&.size <= indent
-
-    # we now know that we have gone one level deeper, where we expect
-    # to find either an instance variable or the value for one of the
-    # special classes: Float, Fixednum, Bignum, String, Array
-
-#    while((line = a.shift) =~ /^*/
-    
-    
-
   end
 
 end
