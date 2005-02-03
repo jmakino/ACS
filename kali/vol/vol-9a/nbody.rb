8fba11302bef7781115f77568eb8022ea10d973f
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2005-02-03 11:43:18 +0000

    copied more files, updated index
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@976 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ kali/vol/vol-9a/nbody.rb
@@ -0,0 +1,27 @@
+require "acs.rb"
+
+class Body
+
+  ACS_OUTPUT_NAME = self.to_s
+
+  attr_accessor :mass, :pos, :vel, :number
+
+  def initialize
+    @mass = 0.0
+    @pos = Vector.new
+    @vel = Vector.new
+  end
+
+end
+
+class Nbody
+
+  ACS_OUTPUT_NAME = self.to_s
+
+  attr_accessor :body
+
+  def initialize
+    @body = []
+  end
+
+end
