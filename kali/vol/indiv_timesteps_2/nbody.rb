ee108dde0b1a50f03ddd1d77fdafa934ed7991a2
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2005-02-28 01:35:08 +0000

    added new directories
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1078 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ kali/vol/indiv_timesteps_2/nbody.rb
@@ -0,0 +1,52 @@
+require "acs"
+
+class Body
+
+# naming convention for instance variables in class Body:
+#
+# @mass    : mass 
+# @pos     : position
+# @vel     : velocity
+# @idn     : identification number of a body, typically used when
+#            numbering the bodies consecutively within an N-body system
+
+end
+
+class Nbody
+
+  ACS_OUTPUT_NAME = self.to_s
+
+  attr_accessor :time, :body             # an array with elements of class Body
+
+  def initialize
+    @body = []
+    @time = 0.0
+  end
+
+  def -(other)
+    if other.class != self.class
+      raise "other.class.name = #{other.class.name} != #{self.class.name}"
+    end
+    if (n = other.body.size) != body.size
+      raise "other.body.size = #{other.body.size} != #{body.size}"
+    end
+    nb = Nbody.new
+    body.each_index do |i|
+      ob = other.body.find{|oi| oi.body_id == body[i].body_id}
+      if ob == nil
+        raise "body_id = #{body[i].body_id} not found in other N-body system"
+      end
+      nb.body[i] = Body.new
+      nb.body[i].pos = body[i].pos - ob.pos
+      nb.body[i].vel = body[i].vel - ob.vel
+    end
+    nb
+  end
+
+  def abs
+    a = 0
+    body.each{ |b| a += b.pos*b.pos + b.vel*b.vel }
+    sqrt a
+  end
+
+end
