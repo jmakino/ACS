356d06614467acd84b3ec36c144d5e6e376ccbc5
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2005-03-04 06:46:37 +0000

    moved directories
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1126 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- lib/clop_rb/20050304_clop.rb
+++ lib/clop_rb/20050304_clop.rb
@@ -1,9 +1,9 @@
+require "acs.rb"
 require "vector.rb"
 
-#:segment start: helperclass
 class Clop_Option
 
-  attr_reader :shortname, :longname, :type,
+  attr_reader :shortname, :longname, :type, :variablename,
               :description, :longdescription, :printname, :defaultvalue
   attr_accessor :valuestring
 
@@ -15,14 +15,14 @@ class Clop_Option
     while s = def_str.shift
       break if parse_single_lines_done?(s)
     end
-    while s = def_str.shift                                                  #9
-      break if s =~ /^\s*$/ and  def_str[0] =~ /^\s*$/                       #9
-      @longdescription += s + "\n"                                           #9
-    end                                                                      #9
+    while s = def_str.shift
+      break if s =~ /^\s*$/ and  def_str[0] =~ /^\s*$/
+      @longdescription += s + "\n"
+    end
   end
 
   def parse_single_lines_done?(s)
-    if s !~ /\s*(\w.*?)\s*\:/                                               #10
+    if s !~ /\s*(\w.*?)\s*\:/
       raise "\n  option definition line has wrong format:\n==> #{s} <==\n"
     end
     name = $1
@@ -33,28 +33,24 @@ class Clop_Option
       when /Long\s+(N|n)ame/
         @longname = content.split[0]
       when /Value\s+(T|t)ype/
-        @type = content.sub(/^\s+/,"").sub(/\s*(#.*|$)/,"")                 #11
-        @valuestring = "false" if @type == "bool"                           #12
+        @type = content.sub(/^\s+/,"").sub(/\s*(#.*|$)/,"")
+        @valuestring = "false" if @type == "bool"
       when /Default\s+(V|v)alue/
         @defaultvalue = content.sub(/^\s+/,"").sub(/\s*(#.*|$)/,"")
         @valuestring = @defaultvalue
-      when /Global\s+(V|v)ariable/
-        @globalname = content.split[0]
+      when /Variable\s+(N|n)ame/
+        @variablename = content.split[0]
       when /Print\s+(N|n)ame/
         @printname = content.sub(/^\s+/,"").sub(/\s*(#.*|$)/,"")
       when /Description/
         @description = content.sub(/^\s+/,"").sub(/\s*(#.*|$)/,"")
       when /Long\s+(D|d)escription/
-        @longdescription = ""                                               #13
-        return true                                                         #13
+        @longdescription = ""
+        return true
       else
         raise "\n  option definition line unrecognized:\n==> #{s} <==\n"
     end
     return false
-  end
-
-  def initialize_global_variable
-    eval("$#{@globalname} = eval_value") if @globalname
   end
 
   def eval_value
@@ -70,7 +66,7 @@ class Clop_Option
       when /^float\s*vector$/
         @valuestring.gsub(/[\[,\]]/," ").split.map{|x| x.to_f}.to_v
       else
-        raise "\n  type \"#{@type}\" is not recognized"
+        raise "\n  type \"#{@type}\" is not recognized\n"
     end
   end
 
@@ -80,74 +76,84 @@ class Clop_Option
   end
 
   def to_s
-    if @type == nil                                                         #14
-      s = @description + "\n"                                               #14
-    elsif @type == "bool"                                                   #15
-      if eval(@valuestring)                                                 #15
-        s = @description + "\n"                                             #15
-      else                                                                  #15
-        s = ""                                                              #15
-      end                                                                   #15
+    if @type == nil
+      s = @description + "\n"
+    elsif @type == "bool"
+      if eval(@valuestring)
+        s = @description + "\n"
+      else
+        s = ""
+      end
     else
-      s = @description                                                      #16
-      s = add_tabs(s, s.size, 4)                                            #16
-      s += ": "                                                             #16
-      if @printname                                                         #16
-        s += @printname                                                     #16
-      else                                                                  #16
-        s += @globalname                                                    #16
-      end                                                                   #16
-      s += " = " unless @printname == ""                                    #17
-      s += "\n  " if @type =~ /^float\s*vector$/                            #18
-      s += "#{eval("$#{@globalname}")}\n"                                   #19
+      s = @description
+      s = add_tabs(s, s.size, 4)
+      s += ": "
+      if defined? @printname
+        s += @printname
+      else
+        s += @variablename
+      end
+      s += " = " unless defined? @printname and @printname == ""
+      s += "\n  " if @type =~ /^float\s*vector$/
+      s += eval_value.to_s + "\n"
     end
     return s
   end
 
 end
-#:segment end:
 
 class Clop
 
-  def initialize(def_str, argv_array = nil)
-    parse_option_definitions(def_str)                                        #8
-    if argv_array
-      parse_command_line_options(argv_array)
-    end
+  def initialize(def_str, argv_array, global_variables_flag)
+    parse_option_definitions(def_str)
+    parse_command_line_options(argv_array)
+    initialize_option_variables
+    initialize_global_variables if global_variables_flag
+    check_required_options
     print_values
   end
 
   def parse_option_definitions(def_str)
-    a = def_str.split("\n")                                                  #1
+    def_str += HELP_DEFINITION_STRING
+    a = def_str.split("\n")
     @options=[]
     while a[0]
-      if a[0] =~ /^\s*$/                                                     #2
-        a.shift                                                              #2
+      if a[0] =~ /^\s*$/
+        a.shift
       else
-        @options.push(Clop_Option.new(a))                                    #3
+        @options.push(Clop_Option.new(a))
       end
     end
   end
 
   def parse_command_line_options(argv_array)
-    while s = argv_array.shift
+    s1 = argv_array.clone
+    while s = s1.shift
       if s == "-h"
-        parse_help(argv_array, false)
+        parse_help(s1, false)
         exit
       elsif s == "--help"
-        parse_help(argv_array, true)
+        parse_help(s1, true)
         exit
-      elsif i = find_option(s)
+      elsif s == "---help"
+        print_help(true, 0)
+        exit
+      end
+    end
+    while s = argv_array.shift
+      if i = find_option(s)
         parse_option(i, s, argv_array)
       else
         raise "\n  option \"#{s}\" not recognized; try \"-h\" or \"--help\"\n"
       end
     end
-    initialize_global_variables
   end
 
   def print_values
-    @options.each{|x| STDERR.print x.to_s}
+    STDERR.print "==> ", @options[0].description, " <==\n"
+    for i in 1...@options.size - 2   # exclude header & help (first & last two)
+      STDERR.print @options[i].to_s
+    end
   end
 
   def find_option(s)
@@ -155,7 +161,7 @@ class Clop
     @options.each_index do |x|
       i = x if s == @options[x].longname
       if @options[x].shortname
-        i = x if s =~ Regexp.new(@options[x].shortname) and $` == ""         #4
+        i = x if s =~ Regexp.new(@options[x].shortname) and $` == ""
       end
     end
     return i
@@ -166,25 +172,40 @@ class Clop
       @options[i].valuestring = "true"
       return
     end
-    if s =~ /^-[^-]/ and (value = $') =~ /\w/                                #6
-      @options[i].valuestring = value                                        #6
+    if s =~ /^-[^-]/ and (value = $') =~ /\w/
+      @options[i].valuestring = value
     else
-      unless @options[i].valuestring = argv_array.shift                      #5
-        raise "\n  option \"#{s}\" requires a value, but no value given;\n" +#5
-              "  option description: #{@options[i].description}\n"           #5
+      unless @options[i].valuestring = argv_array.shift
+        raise "\n  option \"#{s}\" requires a value, but no value given;\n" +
+              "  option description: #{@options[i].description}\n"
       end
     end
-    if @options[i].type =~ /^float\s*vector$/                                #7
-      while (@options[i].valuestring !~ /\]/)                                #7
-        @options[i].valuestring += " " + argv_array.shift                    #7
+    if @options[i].type =~ /^float\s*vector$/
+      while (@options[i].valuestring !~ /\]/)
+        @options[i].valuestring += " " + argv_array.shift
       end
     end
   end
 
-  def initialize_global_variables
-    @options.each{|x| x.initialize_global_variable}
-    check_required_options
+#  def initialize_variables(magic)                     # for connoisseurs . . .
+#    @options.each{|x| eval("#{magic}#{x.variablename}=x.eval_value") if
+#                                                               x.variablename}
+#  end
+
+  def initialize_option_variables
+    @options.each{|x| eval("@#{x.variablename}=x.eval_value") if x.variablename}
   end    
+
+  def initialize_global_variables
+    @options.each{|x| eval("$#{x.variablename}=x.eval_value") if x.variablename}
+  end    
+
+  def mk_reader
+    s = "class Clop \n attr_reader"
+    @options.each{|x| s += " :#{x.variablename}," if x.variablename}
+    s.chop!
+    s + "\n end"
+  end
 
   def check_required_options
     options_missing = 0
@@ -208,9 +229,11 @@ class Clop
   def parse_help(argv_array, long)
     all = true
     while s = argv_array.shift
+      all = false
       if i = find_option(s)
-        all = false
         print_help(long, i)
+      else
+        print_help_warning(s)
       end
     end
     print_help(long) if all
@@ -224,19 +247,23 @@ class Clop
     end
   end
 
+  def print_help_warning(s)
+    STDERR.print "WARNING : ", s, " : ==> this option is not recognized <==\n"
+  end
+
   def help_string(option, long_flag)
     s = ""
-    if option.type
+    if option.type or option.longname =~ /-+-help/
       s += option_name_string(option)
     end
-    if option.type or not long_flag                                         #20
-      s += "#{option.description}"                                          #20
-      s += default_value_string(option)                                     #20
-      s += "\n"                                                             #20
-    end                                                                     #20
-    if long_flag                                                            #21
-      s += "\n#{option.longdescription}\n"                                  #21
-    end                                                                     #21
+    if option.type or option.longname =~ /-+-help/ or not long_flag
+      s += "#{option.description}"
+      s += default_value_string(option)
+      s += "\n"
+    end
+    if long_flag
+      s += "\n#{option.longdescription}\n"
+    end
     return s
   end
 
@@ -256,15 +283,55 @@ class Clop
     if option.type and option.type != "bool"
       reference_size = "#{option.description}".size + 2
       s = option.add_tabs(s, reference_size, 4)
-      s += " [default: #{option.defaultvalue}]"                             #22
+      s += " [default: #{option.defaultvalue}]"
     end
     return s
   end
 
+  HELP_DEFINITION_STRING = <<-END
+
+
+  Short name: 		-h
+  Long name:		--help
+  Description:		Help facility
+  Long description:
+    When providing the command line option -h, followed by one or more
+    options, a one-line summary of each of the options will be printed.
+
+    These options can be specified in either short or long form, i.e
+    typing "some_command -h -x" or "some_command -h --extra" will produce
+    the same output, if "-x" and "--extra" invoke the same option.
+
+    When providing the command line option -h, with nothing else following,
+    a one-line summary of all options will be printed.
+
+    When providing the command line option --help, instead of -h, the same
+    actions occur, the only difference being that instead of a one-liner,
+    a longer description will be printed.
+
+    Anything that appears on the command line between the name of the
+    program and "-h" or "--help" will be ignored.  For example, typing
+    "some_command gobbledygook -h -x" will produce the same output as
+    typing "some_command -h -x"
+
+
+  Long name:		---help
+  Description:		Program description (the header part of --help)
+  Long description:
+    This option prints only the header part of what would be printed with
+    the option --help, without printing all the information about specific
+    option.  In other words, it provides only the general information about
+    the program itself.
+
+
+  END
+
 end
 
-def parse_command_line(def_str)
-  Clop.new(def_str, ARGV)
+def parse_command_line(def_str, global_variables_flag = false)
+  c = Clop.new(def_str, ARGV, global_variables_flag)
+  eval(c.mk_reader)
+  c
 end
 
 if __FILE__ == $0
@@ -287,7 +354,7 @@ if __FILE__ == $0
   Long name:		--softening_length
   Value type:		float              # double/real/...
   Default value: 	0.0
-  Global variable: 	eps                # any comment allowed here
+  Variable name: 	eps                # any comment allowed here
   Description:		Softening length   # and here too
   Long description:                        # and even here
     This option sets the softening length used to calculate the force
@@ -299,7 +366,7 @@ if __FILE__ == $0
   Long name:		--end_time
   Value type:		float
   Default value:	10
-  Global variable:	t_end
+  Variable name:	t_end
   Print name:		t
   Description:		Time to stop integration
   Long description:
@@ -310,7 +377,7 @@ if __FILE__ == $0
   Long name:		--number_of_particles
   Value type:		int
   Default value:	none
-  Global variable:	n_particles
+  Variable name:	n_particles
   Print name:		N
   Description:		Number of particles
   Long description:
@@ -320,7 +387,7 @@ if __FILE__ == $0
   Short name:		-x
   Long name:  		--extra_diagnostics
   Value type:  		bool
-  Global variable:	xdiag
+  Variable name:	xdiag
   Description:		Extra diagnostics
   Long description:
     The following extra diagnostics will be printed:
@@ -333,7 +400,7 @@ if __FILE__ == $0
   Long name:		--shift_velocity
   Value type:		float vector          # numbers in between [ ] brackets
   Default value:	[3, 4, 5]
-  Global variable:	vcom
+  Variable name:	vcom
   Description:		Shifts center of mass velocity
   Long description:
     The center of mass of the N-body system will be shifted by this amount.
@@ -347,7 +414,7 @@ if __FILE__ == $0
   Long name:		--output_file_name
   Value type:		string
   Default value:	none
-  Global variable:	output_file_name
+  Variable name:	output_file_name
   Print name:	 	                      # no name, hence name suppressed
   Description:		Name of the outputfile
   Long description:
@@ -359,7 +426,7 @@ if __FILE__ == $0
   Long name:		--star_type             # no short option given here
   Value type:		string
   Default value:	star: MS                # parser cuts only at first ":"
-  Global variable:	star_type
+  Variable name:	star_type
   Description:		Star type
   Long description:
     This options allows you to specify that a particle is a star, of a
@@ -374,6 +441,9 @@ if __FILE__ == $0
 
   END
 
-  parse_command_line(options_definition_string)
+  clop = parse_command_line(options_definition_string, false)
 
+  print "\nclop.rb: testing automatic generation of attribute readers:\n"
+  print "  clop.t_end = ", clop.t_end, "\n"
+  
 end
