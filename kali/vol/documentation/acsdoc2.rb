5872a833afe32c9ff842e3cec0ba482d957a84dd
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2005-11-18 00:21:44 +0000

    working-change-for-acsdoc2
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1374 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/vol/documentation/acsdoc2.rb
+++ kali/vol/documentation/acsdoc2.rb
@@ -878,7 +878,7 @@ module Acsdoc
       raise "Failed to create the jpeg file #{texbase}.jpeg"
     end
     @@imgcount+=1
-    "link:../#{imgname}"
+    "<IMG SRC=#{imgname}"
   end
 
   def process_tex_inlines(s,instring,dirname)
