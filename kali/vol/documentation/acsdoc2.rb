38f1e36b9e2c0e8f5578a335abeacc0b113bd076
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2005-11-19 02:42:12 +0000

    working-change-for-acsdoc2
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1456 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/vol/documentation/acsdoc2.rb
+++ kali/vol/documentation/acsdoc2.rb
@@ -1676,7 +1676,7 @@ PRE {
 }
 .center  { text-align: center; }
 
-.equation  { margin-left: auto; margin-right: 3px;}
+.equation  { margin-left: auto; margin-right: auto;}
 
 body,td,p { font-family: Verdana, Arial, Helvetica, sans-serif; 
        color: #000010;
