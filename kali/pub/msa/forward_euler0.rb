cb2eb9eaec1f25c7a89e07afb9dc65a28bdbe1b7
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2005-11-24 02:43:25 +0000

    more text
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1540 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/pub/msa/forward_euler0.rb
+++ kali/pub/msa/forward_euler0.rb
@@ -1,21 +1,25 @@
 include Math
 
-r = [1, 0, 0]
-v = [0, 0.5, 0]
+x = 1
+y = 0
+z = 0
+vx = 0
+vy = 0.5
+vz = 0
 dt = 0.01
 
 1000.times{
-  r2 = r[0]*r[0] + r[1]*r[1] + r[2]*r[2]
+  r2 = x*x + y*y + z*z
   r3 = r2 * sqrt(r2)
-  acc[0] = - r[0] / r3
-  acc[1] = - r[1] / r3
-  acc[2] = - r[2] / r3
-  r[0] += v[0]*dt
-  r[1] += v[1]*dt
-  r[1] += v[2]*dt
-  v[0] += acc[0]*dt
-  v[1] += acc[1]*dt
-  v[2] += acc[2]*dt
-  print(r[0], "  ", r[1], "  ", r[2], "  ")
-  print(v[0], "  ", v[1], "  ", v[2], "\n")
+  ax = - x / r3
+  ay = - y / r3
+  az = - z / r3
+  x += vx*dt
+  y += vy*dt
+  y += vz*dt
+  vx += ax*dt
+  vy += ay*dt
+  vz += az*dt
+  print(x, "  ", y, "  ", z, "  ")
+  print(vx, "  ", vy, "  ", vz, "\n")
 }
