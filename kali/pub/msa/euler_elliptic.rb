7e362e6c8ec41245668381392a7f344947c7bcb0
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2006-02-20 17:29:29 +0000

    more text
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1620 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/pub/msa/euler_elliptic.rb
+++ kali/pub/msa/euler_elliptic.rb
@@ -1,4 +1,4 @@
-include Math
+include Math                                #7
 
 x = 1                                       #1
 y = 0                                       #1
@@ -8,18 +8,18 @@ vy = 0.5                                    #1
 vz = 0                                      #1
 dt = 0.01                                   #2
 
-1000.times{
-  r2 = x*x + y*y + z*z
-  r3 = r2 * sqrt(r2)
-  ax = - x / r3
-  ay = - y / r3
-  az = - z / r3
-  x += vx*dt
-  y += vy*dt
-  y += vz*dt
-  vx += ax*dt
-  vy += ay*dt
-  vz += az*dt
-  print(x, "  ", y, "  ", z, "  ")
-  print(vx, "  ", vy, "  ", vz, "\n")
-}
+1000.times{                                 #3
+  r2 = x*x + y*y + z*z                      #6
+  r3 = r2 * sqrt(r2)                        #8
+  ax = - x / r3                             #8
+  ay = - y / r3                             #8
+  az = - z / r3                             #8
+  x += vx*dt                                #4
+  y += vy*dt                                #4
+  z += vz*dt                                #4
+  vx += ax*dt                               #5
+  vy += ay*dt                               #5
+  vz += az*dt                               #5
+  print(x, "  ", y, "  ", z, "  ")          #9
+  print(vx, "  ", vy, "  ", vz, "\n")       #9
+}                                           #3
