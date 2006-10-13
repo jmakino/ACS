86977ca39fcc7fa8cf4edc6a7d183a8680bf6617
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2006-10-13 17:39:59 +0000

    Friday October 13, 2006
    No superstitions.
    
    Stan's first commit.  Should contain two ruby files, one image (gif) and 
    a modified ch01.ok file.
    
    
    
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1777 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ kali/pub/msd/leap_frog.rb
@@ -0,0 +1,102 @@
+#simple ruby 2 body leapfrog  code
+
+require "opengl"
+require "glut"
+include Math
+
+#initial values for position, velocity components, and time increments
+x = 1.0
+y = 0.0
+z = 0.0
+vx = 0.0
+vy = 0.5
+vz = 0.0
+m1 = 0.6
+m2 = 1 - m1
+r2 = x*x + y*y + z*z
+r3 = r2*sqrt(r2)
+ax = -x/r3
+ay = -y/r3
+az = -z/r3
+
+#This value keeps a smooth orbit on my workstation
+#Smaller values slow down the orbit, higher values speed things up
+dt = 0.00025
+
+#calculate new positions
+orbits = Proc.new{
+	
+	vx += 0.5*ax*dt
+	vy += 0.5*ay*dt
+	vz += 0.5*az*dt
+	x += vx*dt
+	y += vy*dt
+	z += vz*dt
+	r2 = x*x + y*y + z*z
+	r3 = r2*sqrt(r2)
+	ax = -x/r3
+	ay = -y/r3
+	az = -z/r3
+	vx += 0.5*ax*dt
+	vy += 0.5*ay*dt
+	vz += 0.5*az*dt
+	#send x,y,z to the display
+	GLUT.PostRedisplay()
+}
+
+#display the "stars" in their orbits around each other
+display = Proc.new {
+   GL.Clear(GL::COLOR_BUFFER_BIT)
+   GL.PushMatrix()
+	GL.Translate(m1*x, m1*y, z)
+	#orange color for less massive star
+	GL.Color3ub(245, 150, 30)
+	GLUT.SolidSphere(0.02, 10.0, 10.0)
+   GL.PopMatrix()
+   GL.PushMatrix()
+	GL.Translate(-m2*x, -m2*y, z)
+	#bright yellow for more massive star
+	GL.Color3ub(240, 230, 100)
+	GLUT.SolidSphere(0.035, 10.0, 10.0)
+   GL.PopMatrix()
+   GLUT.SwapBuffers()
+}
+
+#black background and a somewhat yellow star
+def init
+   GL.ClearColor(0.0, 0.0, 0.0, 0.0)
+end
+
+#window reshape stuff, including view perspective
+reshape = Proc.new {|w, h|
+   GL.Viewport(0, 0, w,  h)
+   GL.MatrixMode(GL::PROJECTION)
+   GL.LoadIdentity()
+   GLU.Perspective(45.0, 1.0, 1.0, 10.0)
+   GLU.LookAt(0.0, 0.0, 1.75, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+   GL.MatrixMode(GL::MODELVIEW)
+
+   GL.LoadIdentity()
+}
+
+keyboard = Proc.new{|key, x ,y|
+	if key == 27  # 27 = 'Esc' key
+		exit(0)
+	elsif key == ?q
+		exit(0)
+	end
+}
+
+#Initialize OpenGL and GLUT
+GLUT.Init
+GLUT.InitDisplayMode(GLUT::DOUBLE | GLUT::RGB)
+GLUT.InitWindowSize(500, 500)
+GLUT.InitWindowPosition(100, 100)
+GLUT.CreateWindow("Leap Frog MSA")
+GLUT.DisplayFunc(display)
+GLUT.ReshapeFunc(reshape)
+GLUT.IdleFunc(orbits)
+GLUT.KeyboardFunc(keyboard)
+
+init();
+GLUT.MainLoop()
---
+++ kali/pub/msd/ogl1.rb
@@ -0,0 +1,19 @@
+# First Ruby OpenGL Program
+# ogl1.rb
+
+require "opengl"
+require "glut"
+
+display = Proc.new{
+	GL.Clear(GL::COLOR_BUFFER_BIT)
+	GLUT.WireTeapot(0.5)
+	GL.Flush()
+}
+
+GLUT.Init
+GLUT.InitDisplayMode(GLUT::SINGLE | GLUT::RGB)
+GLUT.InitWindowSize(250, 250)
+GLUT.InitWindowPosition(100, 100)
+GLUT.CreateWindow("My First OGL Program")
+GLUT.DisplayFunc(display)
+GLUT.MainLoop()
