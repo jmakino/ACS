c47646c97f8cb37ca9e2673cb31e57677842dbf7
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2006-10-13 06:22:07 +0000

    clarifications triggered by Steve Geiss critique
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1776 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/pub/msa/euler_two_body.rb
+++ kali/pub/msa/euler_two_body.rb
@@ -8,18 +8,20 @@ vy = 0.5
 vz = 0
 dt = 0.00001
 
-m1 = 0.6                                            #4
-m2 = 1 - m1                                         #4
+m1 = 0.6                                                        #4
+m2 = 1 - m1                                                     #4
 
-def print2(m1,m2,x,y,z,vx,vy,vz)                    #1
-  print(-m2*x, "  ", -m2*y, "  ", -m2*z, "  ")      #1
-  print(-m2*vx, "  ", -m2*vy, "  ", -m2*vz, "\n")   #1
-  print(m1*x, "  ", m1*y, "  ", m1*z, "  ")         #1
-  print(m1*vx, "  ", m1*vy, "  ", m1*vz, "\n")      #1
-end                                                 #1
+def print2(m1,m2,x,y,z,vx,vy,vz)                                #1
+  mfrac1 = m1/(m1+m2)
+  mfrac2 = m2/(m1+m2)
+  print(-mfrac2*x, "  ", -mfrac2*y, "  ", -mfrac2*z, "  ")      #1
+  print(-mfrac2*vx, "  ", -mfrac2*vy, "  ", -mfrac2*vz, "\n")   #1
+  print(mfrac1*x, "  ", mfrac1*y, "  ", mfrac1*z, "  ")         #1
+  print(mfrac1*vx, "  ", mfrac1*vy, "  ", mfrac1*vz, "\n")      #1
+end                                                             #1
 
-print2(m1,m2,x,y,z,vx,vy,vz)                        #2
-1000000.times{|i|                                   #2
+print2(m1,m2,x,y,z,vx,vy,vz)                                    #2
+1000000.times{|i|                                               #2
   r2 = x*x + y*y + z*z
   r3 = r2 * sqrt(r2)
   ax = - x / r3
@@ -31,5 +33,5 @@ print2(m1,m2,x,y,z,vx,vy,vz)                        #2
   vx += ax*dt
   vy += ay*dt
   vz += az*dt
-  print2(m1,m2,x,y,z,vx,vy,vz) if i%1000 == 999     #3
+  print2(m1,m2,x,y,z,vx,vy,vz) if i%1000 == 999                 #3
 }
