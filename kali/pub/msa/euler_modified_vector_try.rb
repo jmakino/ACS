144bdc9a5297474eb24d71f313412766c64725b7
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2006-10-20 11:53:11 +0000

    more text improvements, and extra codes for chapter 16
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1787 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/pub/msa/euler_modified_vector_try.rb
+++ kali/pub/msa/euler_modified_vector_try.rb
@@ -7,21 +7,22 @@ def print_pos_vel(r,v)
   print "\n"
 end
 
-def step_pos_vel(r,v,dt)
-  r2 = r*r
-  r3 = r2 * sqrt(r2)
-  a = -r/r3
-  [r + v*dt, v + a*dt]
-end
-
 r = [1, 0, 0].to_v
 v = [0, 0.5, 0].to_v
 dt = 0.01
 
 print_pos_vel(r,v)
 1000.times{
-  r1, v1 = step_pos_vel(r,v,dt)
-  r2, v2 = step_pos_vel(r1,v2,dt)
+  r2 = r*r
+  r3 = r2 * sqrt(r2)
+  a = -r/r3
+  r1 = r + v*dt                     #1
+  v1 = v + a*dt
+  r12 = r1*r1
+  r13 = r12 * sqrt(r12)
+  a1 = -r1/r13
+  r2 = r1 + v1*dt
+  v2 = v1 + a1*dt
   r = 0.5 * ( r + r2 )
   v = 0.5 * ( v + v2 )
   print_pos_vel(r,v)
