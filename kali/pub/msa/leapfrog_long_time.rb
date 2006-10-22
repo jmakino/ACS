4543a62bd2d8063fc4a70514448bcb34f417dd4d
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2006-10-22 04:51:58 +0000

    two new codes, and more text
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1794 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ kali/pub/msa/leapfrog_long_time.rb
@@ -0,0 +1,29 @@
+require "vector.rb"
+include Math
+
+def print_pos_vel(r,v)
+  r.each{|x| print(x, "  ")}
+  v.each{|x| print(x, "  ")}
+  print "\n"
+end
+
+def acc(r)
+  r2 = r*r
+  r3 = r2 * sqrt(r2)
+  -r/r3
+end
+
+r = [1, 0, 0].to_v
+v = [0, 0.5, 0].to_v
+dt = 0.01
+print_pos_vel(r,v)
+
+a = acc(r)
+
+10000.times{                  #1
+  v += 0.5*a*dt
+  r += v*dt
+  a = acc(r)
+  v += 0.5*a*dt
+  print_pos_vel(r,v)
+}
