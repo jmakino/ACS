230eb9607dbf9a1d5bcd95b0599651abc674084b
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2006-10-23 05:02:03 +0000

    two new codes, and more text
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1796 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/pub/msa/euler_modified_vector_try3.rb
+++ kali/pub/msa/euler_modified_vector_try3.rb
@@ -15,16 +15,19 @@ print_pos_vel(r,v)
 1000.times{
   r0 = r
   v0 = v
+
   r02 = r0*r0
   r03 = r02 * sqrt(r02)
   a0 = -r0/r03
   r1 = r0 + v0*dt
   v1 = v0 + a0*dt
+
   r12 = r1*r1
   r13 = r12 * sqrt(r12)
   a1 = -r1/r13
   r2 = r1 + v1*dt
   v2 = v1 + a1*dt
+
   r = 0.5 * ( r0 + r2 )
   v = 0.5 * ( v0 + v2 )
   print_pos_vel(r,v)
---
+++ kali/pub/msa/leapfrog_backward.rb
@@ -0,0 +1,38 @@
+require "vector.rb"
+include Math
+
+def print_pos_vel(r,v)
+  r.each{|x| print(x, "  ")}
+  v.each{|x| print(x, "  ")}
+  print "\n"
+end
+
+def acc(r)
+  r2 = r*r
+  r3 = r2 * sqrt(r2)
+  -r/r3
+end
+
+r = [1, 0, 0].to_v      #2
+v = [0, 0.5, 0].to_v    #2
+dt = 0.01
+print_pos_vel(r,v)
+
+a = acc(r)
+
+10000.times{            #1
+  v += 0.5*a*dt         #1
+  r += v*dt             #1
+  a = acc(r)            #1
+  v += 0.5*a*dt         #1
+}                       #1
+                        #1
+dt = -dt                #1
+10000.times{            #1
+  v += 0.5*a*dt         #1
+  r += v*dt             #1
+  a = acc(r)            #1
+  v += 0.5*a*dt         #1
+  print_pos_vel(r,v)    #1
+}                       #1
+
