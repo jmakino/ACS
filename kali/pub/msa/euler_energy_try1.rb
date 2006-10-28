512bc33f9ba122103a3b0dc5a17837a92f96f934
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2006-10-28 10:53:13 +0000

    lots of new benchmark codes
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1805 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/pub/msa/euler_energy_try1.rb
+++ kali/pub/msa/euler_energy_try1.rb
@@ -11,11 +11,11 @@ def print_energy(r,v,e0)
   ekin = 0.5*v*v
   epot = -1/sqrt(r*r)
   etot = ekin + epot
-  STDERR.print "  E_kin = #{sprintf("%.3g", ekin)}, "
-  STDERR.print "E_pot = #{sprintf("%.3g", epot)}; "
-  STDERR.print "E_tot = #{sprintf("%.3g", etot)}\n"
-  STDERR.print "            E_tot - E_init = #{sprintf("%.3g", etot-e0)}, "
-  STDERR.print "(E_tot - E_init) / E_init =#{sprintf("%.3g", (etot-e0)/e0)}\n"
+  STDERR.print "  E_kin = ", sprintf("%.3g, ", ekin)
+  STDERR.print "E_pot = ", sprintf("%.3g; ", epot)
+  STDERR.print "E_tot = ", sprintf("%.3g\n", etot)
+  STDERR.print "            E_tot - E_init = ", sprintf("%.3g, ", etot-e0)
+  STDERR.print "(E_tot - E_init) / E_init = ", sprintf("%.3g\n", (etot-e0)/e0)
 end
 
 r = [1, 0, 0].to_v
---
+++ kali/pub/msa/euler_vector_100000_steps.rb
@@ -0,0 +1,21 @@
+require "vector.rb"
+include Math
+
+def print_pos_vel(r,v)
+  r.each{|x| print(x, "  ")}
+  v.each{|x| print(x, "  ")}
+  print "\n"
+end
+
+r = [1, 0, 0].to_v
+v = [0, 0.5, 0].to_v
+dt = 0.0001
+
+100000.times{
+  r2 = r*r
+  r3 = r2 * sqrt(r2)
+  a = -r/r3
+  r += v*dt
+  v += a*dt
+}
+print_pos_vel(r,v)
