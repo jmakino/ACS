93e4891910b4a51d1e3364b51211d0b23e3ae7c2
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2006-10-28 09:27:07 +0000

    small changes to the code I/O
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1803 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

--- kali/pub/msa/euler_energy.rb
+++ kali/pub/msa/euler_energy.rb
@@ -11,24 +11,25 @@ def print_energy(r,v,e0)
   ekin = 0.5*v*v
   epot = -1/sqrt(r*r)
   etot = ekin + epot
-  STDERR.print "  E_kin = #{sprintf("%.3g", ekin)}, "
-  STDERR.print "E_pot = #{sprintf("%.3g", epot)}; "
-  STDERR.print "E_tot = #{sprintf("%.3g", etot)}\n"
-  STDERR.print "            E_tot - E_init = #{sprintf("%.3g", etot-e0)}, "
-  STDERR.print "(E_tot - E_init) / E_init =#{sprintf("%.3g", (etot-e0)/e0)}\n"
+  STDERR.print "  E_kin = ", sprintf("%.3g, ", ekin)
+  STDERR.print "E_pot = ", sprintf("%.3g; ", epot)
+  STDERR.print "E_tot = ", sprintf("%.3g\n", etot)
+  STDERR.print "            E_tot - E_init = ", sprintf("%.3g, ", etot-e0)
+  STDERR.print "(E_tot - E_init) / E_init = ", sprintf("%.3g\n", (etot-e0)/e0)
 end
 
 r = [1, 0, 0].to_v
 v = [0, 0.5, 0].to_v
 e0 = 0.5*v*v - 1/sqrt(r*r)
 t = 0
-t_end = 10
-STDERR.print "Please provide a value for the time step\n"
+STDERR.print "time step =\n"
 dt = gets.to_f
+STDERR.print "final time =\n"
+t_end = gets.to_f
 print_pos_vel(r,v)
 print_energy(r,v,e0)
 
-while t < t_end
+while t < t_end - 0.5*dt
   r2 = r*r
   r3 = r2 * sqrt(r2)
   a = -r/r3
--- kali/pub/msa/leapfrog_energy.rb
+++ kali/pub/msa/leapfrog_energy.rb
@@ -17,25 +17,26 @@ def print_energy(r,v,e0)
   ekin = 0.5*v*v
   epot = -1/sqrt(r*r)
   etot = ekin + epot
-  STDERR.print "  E_kin = #{sprintf("%.3g", ekin)}, "
-  STDERR.print "E_pot = #{sprintf("%.3g", epot)}; "
-  STDERR.print "E_tot = #{sprintf("%.3g", etot)}\n"
-  STDERR.print "            E_tot - E_init = #{sprintf("%.3g", etot-e0)}, "
-  STDERR.print "(E_tot - E_init) / E_init =#{sprintf("%.3g", (etot-e0)/e0)}\n"
+  STDERR.print "  E_kin = ", sprintf("%.3g, ", ekin)
+  STDERR.print "E_pot = ", sprintf("%.3g; ", epot)
+  STDERR.print "E_tot = ", sprintf("%.3g\n", etot)
+  STDERR.print "            E_tot - E_init = ", sprintf("%.3g, ", etot-e0)
+  STDERR.print "(E_tot - E_init) / E_init = ", sprintf("%.3g\n", (etot-e0)/e0)
 end
 
 r = [1, 0, 0].to_v
 v = [0, 0.5, 0].to_v
 e0 = 0.5*v*v - 1/sqrt(r*r)
 t = 0
-t_end = 10
-STDERR.print "Please provide a value for the time step\n"
+STDERR.print "time step =\n"
 dt = gets.to_f
+STDERR.print "final time =\n"
+t_end = gets.to_f
 print_pos_vel(r,v)
 print_energy(r,v,e0)
 
 a = acc(r)
-while t < t_end
+while t < t_end - 0.5*dt
   v += 0.5*a*dt
   r += v*dt
   a = acc(r)
