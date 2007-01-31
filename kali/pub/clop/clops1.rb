2fe798d35092382da71459b88f861c119190e180
acs <acs@bd19e35d-e7cb-0310-b0fa-9c46eba1bb85>
2007-01-31 04:39:35 +0000

    new volume kali/pub/clop, started by Alf -- Piet
    git-svn-id: svn+ssh://zel2/home/makino/svn_zel2/acs/acs@1844 bd19e35d-e7cb-0310-b0fa-9c46eba1bb85

---
+++ kali/pub/clop/clops1.rb
@@ -0,0 +1,29 @@
+class Clops
+	def initialize(option_definition_str, argv_array=nil)
+		parse_option_definition(option_definition_str)
+		parse_command_line if argv_array
+	end
+	
+	def parse_option_definition(option_definition_str)
+		@option_definition = option_definition_str
+	end
+	
+	attr_reader :option_definition_str
+
+end
+
+
+require 'test/unit'
+class ClopsTests < Test::Unit::TestCase
+
+	SIMPLE_DEFINITION = <<-END
+		Description:  This is checks only the description block at the top.
+		Long Description: This is checks only the description block at the top.
+		  This is a longer, multi-line version of the description of the program.
+	END
+
+	def test_simple_definition_saved
+		c = Clops.new(SIMPLE_DEFINITION)
+		assert_equal(SIMPLE_DEFINITION, c.option_definition_str, "The simple defintion string was not saved.")
+	end
+end
